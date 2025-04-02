060cb85562ad4fe6a5ec63c93536d53767a6e91c
ali mert inal <mert.inal@gmail.com>
2025-04-03 00:01:21 +0200

    BE

---
+++ backend/src/main/java/com/Spring_React/Spring_React/SpringReactApplication.java
@@ -0,0 +1,14 @@
+package com.Spring_React.Spring_React;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactApplication.class, args);
+		System.out.println("başladık!");
+	}
+
+}
